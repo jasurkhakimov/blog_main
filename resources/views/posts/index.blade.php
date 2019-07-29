93974f5a4c5abe3838414add3de4062a39e831fe
Jasur Khakimov <jasur.khakimov9@gmail.com>
2019-07-29 21:06:43 +0500

    changes 5

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> Posts </h1>
+    <h1 class="my-4"> Posts </h1>
     @if(count($posts) > 0)
         @foreach ($posts as $post)
-            <div class="article">
+            <div class="article mb-4">
                 <img src="https://images.pexels.com/photos/177557/pexels-photo-177557.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" class="img-fluid" >
                 <div class="article-body">
                     <div class="article-header">
