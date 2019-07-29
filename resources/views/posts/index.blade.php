0aebf75797ab41703d8fd8dfef80fc7cf82421dc
Jasur Khakimov <jasur.khakimov9@gmail.com>
2019-07-29 19:25:54 +0500

    some changes 3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
                 <img src="https://images.pexels.com/photos/177557/pexels-photo-177557.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" class="img-fluid" >
                 <div class="article-body">
                     <div class="article-header">
-                        {{ $post->title }}
+                        <a href="/posts/{{$post->id}}"> {{ $post->title }} </a>
                     </div>
                     <div class="article-text">
                         {{ $post->text }}
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <a href="/posts" class="btn btn-light"> Go Back </a>
+    <h1> Title </h1>
+    <div>
+            <small> Author:  </small>
+            <small class="ml-auto"> Written on: </small>
+    </div>
+    <hr>
+
+    <div class="">
+        text
+    </div>
+
+@endsection
